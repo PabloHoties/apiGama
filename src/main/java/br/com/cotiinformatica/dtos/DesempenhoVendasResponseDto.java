f435b344fe820e956eb89e76db71173477136688
Lucas-dev23 <Ldelimacardoso.ll@gmail.com>
2024-05-25 17:17:39 -0300

    fix: correção do texto de retorno do chat Gpt

--- src/main/java/br/com/cotiinformatica/dtos/DesempenhoVendasResponseDto.java
+++ src/main/java/br/com/cotiinformatica/dtos/DesempenhoVendasResponseDto.java
@@ -5,5 +5,5 @@ import lombok.Data;
 @Data
 public class DesempenhoVendasResponseDto {
 
-	private String desempenho = "A empresa apresenta um desempenho de vendas muito bom, com receitas altas e despesas bem geridas, resultando em um saldo positivo significativo. Este desempenho sugere uma forte posição financeira e uma capacidade eficiente de geração de receitas.";
+	private String desempenho = "As transações refletem um cenário financeiro positivo, com ganhos substanciais que compensam as perdas. O saldo final de R$ 909.199,91 indica uma posição financeira saudável, resultante de depósitos consideráveis e retiradas relativamente pequenas. Essa análise sugere que a conta teve um crescimento significativo no saldo ao longo do período analisado.";
 }
