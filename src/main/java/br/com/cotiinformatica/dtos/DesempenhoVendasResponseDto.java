4bf5ca0de6d366ef9e1a37a6fb2579d84fa1d8bc
Lucas-dev23 <Ldelimacardoso.ll@gmail.com>
2024-05-25 18:53:31 -0300

    feat: Correção da mensagem de desempenho

--- src/main/java/br/com/cotiinformatica/dtos/DesempenhoVendasResponseDto.java
+++ src/main/java/br/com/cotiinformatica/dtos/DesempenhoVendasResponseDto.java
@@ -5,5 +5,5 @@ import lombok.Data;
 @Data
 public class DesempenhoVendasResponseDto {
 
-	private String desempenho = "As transações refletem um cenário financeiro positivo, com ganhos substanciais que compensam as perdas. O saldo final de R$ 909.199,91 indica uma posição financeira saudável, resultante de depósitos consideráveis e retiradas relativamente pequenas. Essa análise sugere que a conta teve um crescimento significativo no saldo ao longo do período analisado.";
+	private String desempenho = "Os dados indicam uma dinâmica financeira onde grandes entradas esporádicas ajudam a equilibrar as várias saídas, muitas das quais são frequentes e variam em tamanho. A gestão do saldo parece ser crítica, com entradas significativas ocasionalmente revertendo os efeitos das saídas. As transações mostram uma tendência de manutenção de um saldo positivo, embora com variações significativas em determinados períodos. A gestão financeira envolvida parece focada em garantir que grandes entradas compensem as frequentes retiradas, mantendo a saúde financeira ao longo do tempo.";
 }
