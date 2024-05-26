46fddca27106a8ae767af689a5b00cfe8c82ecd1
Lucas-dev23 <Ldelimacardoso.ll@gmail.com>
2024-05-26 11:10:31 -0300

    fix: quebra de linha no dto de desempenho

--- src/main/java/br/com/cotiinformatica/dtos/DesempenhoVendasResponseDto.java
+++ src/main/java/br/com/cotiinformatica/dtos/DesempenhoVendasResponseDto.java
@@ -5,12 +5,12 @@ import lombok.Data;
 @Data
 public class DesempenhoVendasResponseDto {
 
-	String desempenho = "Conclusão<br>"
+	String desempenho = "Conclusão:<br>"
 	        + "A empresa demonstra uma capacidade robusta de gerar receitas, mas enfrenta desafios significativos na gestão de seus gastos e na manutenção de um fluxo de caixa positivo. Para melhorar sua gestão financeira, a empresa deve focar em:<br><br>"
 	        + "Redução de Gastos:<br>"
-	        + "Revisar e possivelmente reduzir os gastos elevados para melhorar a liquidez.<br>"
+	        + "Revisar e possivelmente reduzir os gastos elevados para melhorar a liquidez.<br><br>"
 	        + "Melhoria no Controle Financeiro:<br>"
-	        + "Implementar sistemas e processos mais rigorosos para assegurar que as transações sejam registradas corretamente e refletidas com precisão nos saldos.<br>"
+	        + "Implementar sistemas e processos mais rigorosos para assegurar que as transações sejam registradas corretamente e refletidas com precisão nos saldos.<br><br>"
 	        + "Gestão de Fluxo de Caixa:<br>"
 	        + "Adotar estratégias de gestão de fluxo de caixa para evitar saldos negativos e melhorar a saúde financeira geral da empresa.<br><br>"
 	        + "Em resumo, enquanto a empresa possui uma forte capacidade de geração de receita, a melhoria na gestão de gastos e no controle do fluxo de caixa será crucial para garantir uma gestão financeira mais eficaz e sustentável.";
